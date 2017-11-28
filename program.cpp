d996dde3f4c2438e8e93efde22706eb27c394d69
Ernest Shablinskiy <erik79953@gmail.com>
2017-11-28 22:58:40 +0300

    Snapshot1, program.cpp

---
+++ program.cpp
@@ -0,0 +1,11 @@
+//פאיכ program.cpp
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout << MySum(10, 3) << endl;
+cout << MySub(10, 3) << endl;system("pause");
+return 0;
+}
