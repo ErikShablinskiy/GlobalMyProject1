a045b0820d81ea7cfba46304b1f47d398dbcb414
Ernest Shablinskiy <erik79953@gmail.com>
2017-11-28 23:00:50 +0300

    Snapshot2, program.cpp, funcs.h, funcs.cpp

--- program.cpp
+++ program.cpp
@@ -1,11 +1,12 @@
 //פאיכ program.cpp
 #include "stdafx.h"
 #include <iostream>
-
+#include "funcs.h"
+50
 using namespace std;
 int main()
 {
 cout << MySum(10, 3) << endl;
-cout << MySub(10, 3) << endl;system("pause");
+cout << MySub(10, 3) << endl;
 return 0;
 }
